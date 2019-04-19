a827a7c643ec23630554a5e49459f21c21795ed2
Alex Jackson <alex@alexj.org>
2019-04-19 15:52:02 +0100

    Set up swift pm package specification

--- Package.swift
+++ Package.swift
@@ -1,7 +1,17 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
-    name: "TypedNotification"
+    name: "TypedNotification",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "TypedNotification", targets: ["TypedNotification"])
+    ],
+    targets: [
+        .target(name: "TypedNotification", dependencies: [], path: "Sources/"),
+        .testTarget(name: "TypedNotificationTests", dependencies: ["TypedNotification"], path: "Tests/")
+    ]
 )
